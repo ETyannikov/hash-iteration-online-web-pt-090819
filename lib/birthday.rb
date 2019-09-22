d89360bf44f26b97d98d9d7315aef08b44b3f3f9
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-22 23:19:38 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,9 +6,14 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+def age_appropriate_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
     
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
       puts "You are too old for this." if age > 12
   end
 end
-
